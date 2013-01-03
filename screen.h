a90f6872c650da6a799dad874d39c22daa5e1bcf
Dominic Hamon <dma+github@stripysock.com>
2013-01-03 08:40:39 -0800

    Hex and Dec VGA printing. Color tweaks.

--- screen.h
+++ screen.h
@@ -1,6 +1,8 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
+#include "base.h"
+
 enum Color {
   COLOR_BLACK = 0,
   COLOR_DARK_RED = 1,
@@ -24,6 +26,8 @@ namespace screen {
 
 void putc(char c);
 void puts(const char* s);
+void puth(uint32_t h);
+void putd(uint32_t d);
 
 void clear();
 
