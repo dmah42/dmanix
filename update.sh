76e9b7af1c6a301ee021b50893d8f794da9ea92b
Dominic Hamon <dma+github@stripysock.com>
2013-01-22 13:06:26 -0800

    Renamed mountpoint

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo /sbin/losetup /dev/loop0 floppy.img
-sudo /bin/mount /dev/loop0 /mnt/dmaos_floppy
+sudo /bin/mount /dev/loop0 /mnt/dmanix_floppy
 for f in kernel initrd; do
-  sudo cp $f /mnt/dmaos_floppy/$f
+  sudo cp $f /mnt/dmanix_floppy/$f
 done
 sleep 2
-sudo /bin/umount /mnt/dmaos_floppy
+sudo /bin/umount /mnt/dmanix_floppy
 sudo /sbin/losetup -d /dev/loop0
