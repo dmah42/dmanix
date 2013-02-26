1a44ee83c5a539cb2b2f55d61593e7c667500c6e
Dominic Hamon <dma+github@stripysock.com>
2013-02-26 10:54:23 -0800

    Use correct CXXFLAGS for deps generation.
    Working towards issue #1

--- src/base/assert.h
+++ src/base/assert.h
@@ -7,4 +7,10 @@ void panic(const char*, const  char*, uint32_t);
 #define PANIC(msg)  panic(msg, __FILE__, __LINE__)
 #define ASSERT(b)   if (b) { void(0); } else { PANIC(#b); }
 
+#ifdef DEBUG
+#define DEBUG_ASSERT(b)	ASSERT(b)
+#else
+#define DEBUG_ASSERT(b)	(void)(b);
+#endif
+
 #endif  // ASSERT_H
